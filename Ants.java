fdbbe1f8bd7612b6bed44b525dfc7199bfd8fe53
Mika Smith <mismith@oucs1449.otago.ac.nz>
2017-07-12 17:12:31 +1200

    First commit

---
+++ Ants.java
@@ -0,0 +1,23 @@
+/*
+ * Etude 1: Ants on a Plane
+ * 
+ * Authors: 
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Ants {
+	
+	public Ants(){
+	}
+	
+	public static void main(String[]args){
+		new Ants().start(); 
+	}
+	
+	public void start(){
+		
+	}
+	
+}
